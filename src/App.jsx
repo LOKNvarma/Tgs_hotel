8eed606dceae402cb3381780bb8998e139bae39d
Your Name <you@example.com>
2024-12-10 13:14:00 +0530

    navbar edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -25,4 +26,3 @@ const App = () => {
 };
 
 export default App;
-
